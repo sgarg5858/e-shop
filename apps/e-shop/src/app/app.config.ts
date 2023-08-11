6011de7fa0b1bc8449d9a5c0110623acd35426f9
Sanjay Garg <sgarg5858@gmail.com>
2023-08-11 20:15:31 +0530

    added provideStoreDevtools

--- apps/e-shop/src/app/app.config.ts
+++ apps/e-shop/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -6,6 +6,7 @@ import {
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore,provideState } from '@ngrx/store';
+import { provideStoreDevtools, } from '@ngrx/store-devtools';
 
 import { provideEffects } from '@ngrx/effects';
 import { provideHttpClient } from '@angular/common/http';
@@ -18,5 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideState(categoryFeature),
     provideEffects([CategoryEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ],
 };
